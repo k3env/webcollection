29462dda7a32b5c693cac95da3b7cae5bf519013
Sergey Tulonov <me@k3env.site>
2024-05-15 03:51:41 +0800

    feat: update log handler

--- middlewares/log.go
+++ middlewares/log.go
@@ -1,24 +1,57 @@
 package middlewares
 
 import (
+	"bytes"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/urfave/negroni"
 )
 
-func Log(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		lrw := negroni.NewResponseWriter(w)
-		next.ServeHTTP(lrw, r)
-		log.Printf("WEB: %s - %s%s (%s) - %d", r.Method, r.Host, r.URL.Path, r.Proto, lrw.Status())
-	})
+type LogRow struct {
+	Prefix   string
+	Request  *http.Request
+	Response *LogRowResponse
 }
 
-func LogFunc(next http.HandlerFunc) http.Handler {
+type LogRowResponse struct {
+	StatusCode    int
+	ContentLength int
+}
+
+func (h *LogHandler) Handle(next http.Handler) http.Handler {
+	return h.HandleFunc(next.ServeHTTP)
+}
+
+func (h *LogHandler) HandleFunc(next http.HandlerFunc) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		lrw := negroni.NewResponseWriter(w)
 		next(lrw, r)
-		log.Printf("WEB: %s - %s%s (%s) - %d", r.Method, r.Host, r.URL.Path, r.Proto, lrw.Status())
+		row := &LogRow{
+			Prefix:  "Web",
+			Request: r,
+			Response: &LogRowResponse{
+				StatusCode:    lrw.Status(),
+				ContentLength: lrw.Size(),
+			},
+		}
+		buff := bytes.NewBuffer([]byte(""))
+		h.template.Execute(buff, row)
+		log.Println(string(buff.Bytes()))
 	})
+}
+
+type LogHandler struct {
+	template *template.Template
+}
+
+func NewLogHandler(templateText string) (*LogHandler, error) {
+	tpl, err := template.New("log").Parse(templateText)
+	if err != nil {
+		return nil, err
+	}
+	return &LogHandler{
+		template: tpl,
+	}, nil
 }
